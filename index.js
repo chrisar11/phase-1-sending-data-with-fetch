0c83b8d65f37423660e95de68addb3cf7384c0e8
Christopher Sarmiento <chrisar11@gmail.com>
2022-08-28 12:32:43 -0700

    commit tree 83ba03a836b7adc9ae35494fa21453ab13ebbf39

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const formData = {
+    name: "Byron",
+    email: "Poodle",
+}
+
+const submitData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData),
+};
+
+
+fetch('http://localhost:3000/users', submitData)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarok!");
+        console.log(error.message);
+    })
